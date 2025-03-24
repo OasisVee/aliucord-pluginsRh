ef1ba6bb676a0d7624a38953562111eb68754535
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-24 12:53:19 -0400

    FakeStickers: implemented AnimateApngs into code

--- FakeStickers/build.gradle.kts
+++ FakeStickers/build.gradle.kts
@@ -1,12 +1,16 @@
-version = "1.1.2"
-description = "Posts a sticker as an image if the sticker is unavailable normally (Usually when you don't have Nitro). Lottie stickers are unsupported."
+version = "1.2.0"
+description = "Posts a sticker as an image if the sticker is unavailable normally (Usually when you don't have Nitro). Lottie stickers are unsupported. (Stickers Animate client side only in this fork, no need for AnimateApngs plugin)"
 
 aliucord {
     changelogMedia.set("https://cdn.discordapp.com/stickers/883809297216192573.png")
     changelog.set(
         """
+            # 1.2.0
+            * Implemented AnimateApngs into code
+
             # 1.1.1
             * Support Discord 105.12
+
             # 1.1.0
             * Make sticker picker automatically close after selecting a sticker
             * Do not mark stickers as unusable (monochrome filter)
--- FakeStickers/src/main/java/com/accelerator/plugins/FakeStickers.java
+++ FakeStickers/src/main/java/com/accelerator/plugins/FakeStickers.java
@@ -1,11 +1,14 @@
 package com.accelerator.plugins;
 
 import android.content.Context;
+import android.net.Uri;
+import android.widget.ImageView;
 
 import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.entities.Plugin;
 import com.aliucord.patcher.*;
 
+import com.aliucord.Http;
 import com.aliucord.Logger;
 import com.aliucord.Utils;
 
@@ -15,106 +18,122 @@ import com.discord.widgets.chat.input.WidgetChatInputAttachments$createAndConfig
 import com.discord.widgets.chat.input.sticker.*;
 import com.discord.utilities.stickers.StickerUtils;
 import com.discord.utilities.rest.RestAPI;
-//import com.discord.restapi.*;
 import com.discord.restapi.RestAPIParams;
 import com.discord.models.domain.NonceGenerator;
 import com.discord.utilities.time.ClockFactory;
 import com.aliucord.utils.RxUtils;
 import java.util.Collections;
 import com.discord.stores.StoreStream;
-//import org.json.JSONObject;
-//import com.discord.utilities.analytics.AnalyticSuperProperties;
-//import com.aliucord.Http;
 
-// This class is never used so your IDE will likely complain. Let's make it shut up!
+import com.discord.api.message.embed.EmbedType;
+import com.discord.embed.RenderableEmbedMedia;
+import com.discord.widgets.chat.list.InlineMediaView;
+import com.discord.widgets.media.WidgetMedia;
+import java.util.regex.Pattern;
+
 @SuppressWarnings("unused")
 @AliucordPlugin
 public class FakeStickers extends Plugin {
+    private final Logger logger = new Logger("FakeStickers");
 
     public FakeStickers() {}
 
-	@Override
-	// Called when your plugin is started. This is the place to register command, add patches, etc
-	public void start(Context context) throws Throwable {
-		// add the patch
-		
+    private void initApng(ImageView view, String mediaUrl, Integer w, Integer h) {
+        final var url = mediaUrl
+                .replaceFirst("https://images-ext-.*?\\.discordapp\\.net/external/.*?/https/(?:media|cdn)\\.discordapp\\.(?:net|com)", "https://cdn.discordapp.com")
+                .replace("media.discordapp.net", "cdn.discordapp.com");
 
-		// Do not mark stickers as unsendable (grey overlay)
-		patcher.patch(StickerItem.class.getDeclaredMethod("getSendability"), InsteadHook.returnConstant(StickerUtils.StickerSendability.SENDABLE));
+        Utils.threadPool.execute(() -> {
+            try (var is = new Http.Request(url).execute().stream()) {
+                var drawable = b.l.a.a.a(is, w, h);
+                if (view != null)
+                    Utils.mainThread.post(() -> {
+                        view.setImageDrawable(drawable);
+                        drawable.start();
+                    });
+            } catch (Throwable ignored) { }
+        });
+    }
 
-		//Patch onClick to send sticker
-		patcher.patch(WidgetStickerPicker.class.getDeclaredMethod("onStickerItemSelected", StickerItem.class), new PreHook(param -> {
-			try {
-				// getSendability is patched above to always return SENDABLE so get the real value via reflect
-				if (ReflectUtils.getField(param.args[0], "sendability") == StickerUtils.StickerSendability.SENDABLE) return;
+    @Override
+    public void start(Context context) throws Throwable {
+        patcher.patch(StickerItem.class.getDeclaredMethod("getSendability"), InsteadHook.returnConstant(StickerUtils.StickerSendability.SENDABLE));
+        patcher.patch(WidgetStickerPicker.class.getDeclaredMethod("onStickerItemSelected", StickerItem.class), new PreHook(param -> {
+            try {
+                if (ReflectUtils.getField(param.args[0], "sendability") == StickerUtils.StickerSendability.SENDABLE) return;
 
-				var sticker = ((StickerItem) param.args[0]).getSticker();
+                var sticker = ((StickerItem) param.args[0]).getSticker();
 
-				RestAPIParams.Message message = new RestAPIParams.Message(
-					"https://media.discordapp.net/stickers/"+sticker.d()+sticker.b()+"?size=160",
-					Long.toString(NonceGenerator.computeNonce(ClockFactory.get())),
-					null,
-					null,
-					Collections.emptyList(),
-					null,
-					new RestAPIParams.Message.AllowedMentions(
-							Collections.emptyList(),
-							Collections.emptyList(),
-							Collections.emptyList(),
-							false
-					),
-					null,
-					null
-				);
-				new Logger("FakeStickers").debug(message.toString());
-				Utils.threadPool.execute(() -> {
-					//Subscriptions in Java, because you can't do msg.subscribe() like in Kotlin
-					RxUtils.subscribe(
-							RestAPI.getApi().sendMessage(StoreStream.getChannelsSelected().getId(), message),
-							RxUtils.createActionSubscriber(zz -> {})
-					);
-				});
+                RestAPIParams.Message message = new RestAPIParams.Message(
+                    "https://media.discordapp.net/stickers/"+sticker.d()+sticker.b()+"?size=160",
+                    Long.toString(NonceGenerator.computeNonce(ClockFactory.get())),
+                    null,
+                    null,
+                    Collections.emptyList(),
+                    null,
+                    new RestAPIParams.Message.AllowedMentions(
+                            Collections.emptyList(),
+                            Collections.emptyList(),
+                            Collections.emptyList(),
+                            false
+                    ),
+                    null,
+                    null
+                );
+                logger.debug(message.toString());
+                Utils.threadPool.execute(() -> {
+                    RxUtils.subscribe(
+                            RestAPI.getApi().sendMessage(StoreStream.getChannelsSelected().getId(), message),
+                            RxUtils.createActionSubscriber(zz -> {})
+                    );
+                });
+                param.setResult(null);
+                var stickerListener = (WidgetChatInputAttachments$createAndConfigureExpressionFragment$stickerPickerListener$1)
+                        ReflectUtils.getField(param.thisObject, "stickerPickerListener");
+                WidgetChatInputAttachments.access$getFlexInputFragment$p(stickerListener.this$0).s.hideExpressionTray();
+            } catch (Throwable e) {
+                logger.error("Error in sticker patch", e);
+            }
+        }));
+        
+        int previewResId = Utils.getResId("inline_media_image_preview", "id");
+        int mediaResId = Utils.getResId("media_image", "id");
+        var updateUI = InlineMediaView.class.getDeclaredMethod("updateUI", RenderableEmbedMedia.class, String.class, EmbedType.class, Integer.class, Integer.class, String.class);
+        patcher.patch(updateUI, new Hook(param -> {
+            var media = (RenderableEmbedMedia) param.args[0];
+            if (media == null || !media.a.endsWith(".png")) return;
+            var url = media.a.replace("media.discordapp.net", "cdn.discordapp.com");
+            var binding = InlineMediaView.access$getBinding$p((InlineMediaView) param.thisObject);
+            var view = (ImageView) binding.getRoot().findViewById(previewResId);
+            initApng(view, url, media.b, media.c);
+        }));
+        var pattern = Pattern.compile("\\.png(?:\\?width=(\\d+)&height=(\\d+))?");
 
-				// Skip original method
-				param.setResult(null);
+        var uriField = WidgetMedia.class.getDeclaredField("imageUri");
+        uriField.setAccessible(true);
+        var getFormattedUrl = WidgetMedia.class.getDeclaredMethod("getFormattedUrl", Context.class, Uri.class);
+        getFormattedUrl.setAccessible(true);
 
-				// Dismiss sticker picker
-				var stickerListener = (WidgetChatInputAttachments$createAndConfigureExpressionFragment$stickerPickerListener$1) // What a classname jeez
-						ReflectUtils.getField(param.thisObject, "stickerPickerListener");
-				//.s here is FlexInputFragment's FlexInputViewModel property (obfuscated to s)
-				WidgetChatInputAttachments.access$getFlexInputFragment$p(stickerListener.this$0).s.hideExpressionTray();
-			} catch (Throwable ignored) {
-			}
-		}));
-	}
+        patcher.patch(WidgetMedia.class.getDeclaredMethod("configureMediaImage"), new Hook(param -> {
+            try {
+                var widgetMedia = (WidgetMedia) param.thisObject;
+                var url = (String) getFormattedUrl.invoke(widgetMedia, widgetMedia.requireContext(), uriField.get(widgetMedia));
+                if (url == null) return;
+                var match = pattern.matcher(url);
+                if (match.find()) {
+                    String w = match.group(1);
+                    String h = match.group(2);
+                    var view = (ImageView) WidgetMedia.access$getBinding$p(widgetMedia).getRoot().findViewById(mediaResId);
+                    initApng(view, url, w != null ? Integer.parseInt(w) : null, h != null ? Integer.parseInt(h) : null);
+                }
+            } catch (Throwable e) {
+                logger.error("Error in APNG widget media patch", e);
+            }
+        }));
+    }
 
-	@Override
-	// Called when your plugin is stopped
-	public void stop(Context context) {
-		// Remove all patches
-		patcher.unpatchAll();
-	}
-}
-
-/*
-This plugin was mainly written as a learning exercise in order to
-figure out how to send links on tap since it would be required to
-send custom stickers. Whether or not custom stickers will ever
-happen remains to be seen...
-
-
-Copyright (C) Rhythm Lunatic 2021
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+    @Override
+    public void stop(Context context) {
+        patcher.unpatchAll();
+    }
+                      }
