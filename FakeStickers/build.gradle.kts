b50c6356e6f1825a82e8ad588692e78d8de884c8
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-04-01 03:08:48 -0400

    Update build.gradle.kts

--- FakeStickers/build.gradle.kts
+++ FakeStickers/build.gradle.kts
@@ -1,10 +1,14 @@
-version = "1.1.2"
-description = "Posts a sticker as an image if the sticker is unavailable normally (Usually when you don't have Nitro). Lottie stickers are unsupported."
+version = "1.2.0"
+description = "Posts a sticker as an image if the sticker is unavailable normally (Usually when you don't have Nitro). Sticker sizes are now configurable. Lottie stickers are unsupported."
 
 aliucord {
     changelogMedia.set("https://cdn.discordapp.com/stickers/883809297216192573.png")
     changelog.set(
         """
+            # 1.2.0
+            * Added configurable sticker sizes
+            * Improved size handling for sticker images
+            
             # 1.1.1
             * Support Discord 105.12
             # 1.1.0
