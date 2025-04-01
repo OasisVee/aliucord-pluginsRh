2aab94dd91d26737945c9e1b363f5f08278fa415
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-04-01 03:27:58 -0400

    Update FakeStickers.java

--- FakeStickers/src/main/java/com/accelerator/plugins/FakeStickers.java
+++ FakeStickers/src/main/java/com/accelerator/plugins/FakeStickers.java
@@ -1,144 +1,120 @@
 package com.accelerator.plugins;
 
 import android.content.Context;
-import android.view.View;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.*;
 
-import com.aliucord.Http;
-import com.aliucord.Utils;
 import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.entities.Plugin;
-import com.aliucord.patcher.InsteadHook;
-import com.aliucord.patcher.PreHook;
-import com.aliucord.api.SettingsAPI;
-import com.aliucord.utils.ReflectUtils;
+import com.aliucord.patcher.*;
 
+import com.aliucord.Logger;
+import com.aliucord.Utils;
+
+import com.aliucord.utils.ReflectUtils;
+import com.discord.widgets.chat.input.WidgetChatInputAttachments;
+import com.discord.widgets.chat.input.WidgetChatInputAttachments$createAndConfigureExpressionFragment$stickerPickerListener$1;
 import com.discord.widgets.chat.input.sticker.*;
 import com.discord.utilities.stickers.StickerUtils;
 import com.discord.utilities.rest.RestAPI;
+//import com.discord.restapi.*;
 import com.discord.restapi.RestAPIParams;
 import com.discord.models.domain.NonceGenerator;
 import com.discord.utilities.time.ClockFactory;
 import com.aliucord.utils.RxUtils;
 import java.util.Collections;
 import com.discord.stores.StoreStream;
-import java.lang.reflect.Method;
+//import org.json.JSONObject;
+//import com.discord.utilities.analytics.AnalyticSuperProperties;
+//import com.aliucord.Http;
 
+// This class is never used so your IDE will likely complain. Let's make it shut up!
 @SuppressWarnings("unused")
 @AliucordPlugin
 public class FakeStickers extends Plugin {
 
-    private static final int DEFAULT_STICKER_SIZE = 240;
-    private int stickerSize;
+    public FakeStickers() {}
 
-    private void initApngSticker(ImageView view, String stickerUrl, Integer w, Integer h) {
-        final var url = stickerUrl
-                // Replace media domain with cdn to ensure APNG support
-                .replace("media.discordapp.net", "cdn.discordapp.com");
+	@Override
+	// Called when your plugin is started. This is the place to register command, add patches, etc
+	public void start(Context context) throws Throwable {
+		// add the patch
+		
 
-        Utils.threadPool.execute(() -> {
-            try (var is = new Http.Request(url).execute().stream()) {
-                var drawable = b.l.a.a.a(is, w != null ? w : stickerSize, h != null ? h : stickerSize);
-                if (view != null)
-                    Utils.mainThread.post(() -> {
-                        view.setImageDrawable(drawable);
-                        drawable.start();
-                    });
-            } catch (Throwable ignored) { }
-        });
-    }
+		// Do not mark stickers as unsendable (grey overlay)
+		patcher.patch(StickerItem.class.getDeclaredMethod("getSendability"), InsteadHook.returnConstant(StickerUtils.StickerSendability.SENDABLE));
 
-    @Override
-    public void start(Context context) throws Throwable {
-        // Load sticker size from settings
-        stickerSize = settings.getInt("stickerSize", DEFAULT_STICKER_SIZE);
+		//Patch onClick to send sticker
+		patcher.patch(WidgetStickerPicker.class.getDeclaredMethod("onStickerItemSelected", StickerItem.class), new PreHook(param -> {
+			try {
+				// getSendability is patched above to always return SENDABLE so get the real value via reflect
+				if (ReflectUtils.getField(param.args[0], "sendability") == StickerUtils.StickerSendability.SENDABLE) return;
 
-        // Add settings tab
-        settingsTab = new SettingsTab(
-            FakeStickersSettings.class,
-            SettingsTab.Type.BOTTOM_SHEET
-        ).withArgs(settings);
+				var sticker = ((StickerItem) param.args[0]).getSticker();
 
-        // Do not mark stickers as unsendable (grey overlay)
-        patcher.patch(StickerItem.class.getDeclaredMethod("getSendability"), InsteadHook.returnConstant(StickerUtils.StickerSendability.SENDABLE));
+				RestAPIParams.Message message = new RestAPIParams.Message(
+					"https://media.discordapp.net/stickers/"+sticker.d()+sticker.b()+"?size=160",
+					Long.toString(NonceGenerator.computeNonce(ClockFactory.get())),
+					null,
+					null,
+					Collections.emptyList(),
+					null,
+					new RestAPIParams.Message.AllowedMentions(
+							Collections.emptyList(),
+							Collections.emptyList(),
+							Collections.emptyList(),
+							false
+					),
+					null,
+					null
+				);
+				new Logger("FakeStickers").debug(message.toString());
+				Utils.threadPool.execute(() -> {
+					//Subscriptions in Java, because you can't do msg.subscribe() like in Kotlin
+					RxUtils.subscribe(
+							RestAPI.getApi().sendMessage(StoreStream.getChannelsSelected().getId(), message),
+							RxUtils.createActionSubscriber(zz -> {})
+					);
+				});
 
-        // Patch StickerViewHolder to support animated stickers and configurable size
-        patcher.patch(StickerViewHolder.class.getDeclaredMethod("configureSticker", Object.class), new PreHook(param -> {
-            try {
-                var stickerItem = (StickerItem) param.args[0];
-                var sticker = stickerItem.getSticker();
-                
-                // Get the binding and StickerView via reflection
-                Object binding = ReflectUtils.getField(param.thisObject, "binding");
-                Object stickerView = ReflectUtils.getField(binding, "b");
-                
-                // Set custom sticker size
-                LayoutParams layoutParams = ((View) stickerView).getLayoutParams();
-                layoutParams.height = stickerSize;
-                layoutParams.width = stickerSize;
-                ((View) stickerView).setLayoutParams(layoutParams);
+				// Skip original method
+				param.setResult(null);
 
-                // Use reflection to set alpha
-                Method setAlphaMethod = stickerView.getClass().getMethod("setAlpha", float.class);
-                setAlphaMethod.invoke(stickerView, 1.0f);
-                
-                // Add animation support for PNG/APNG stickers
-                if (sticker != null) {
-                    var stickerUrl = "https://media.discordapp.net/stickers/" + sticker.d() + sticker.b() + "?size=" + stickerSize;
-                    initApngSticker((ImageView) stickerView, stickerUrl, stickerSize, stickerSize);
-                }
-            } catch (Throwable ignored) {}
-        }));
+				// Dismiss sticker picker
+				var stickerListener = (WidgetChatInputAttachments$createAndConfigureExpressionFragment$stickerPickerListener$1) // What a classname jeez
+						ReflectUtils.getField(param.thisObject, "stickerPickerListener");
+				//.s here is FlexInputFragment's FlexInputViewModel property (obfuscated to s)
+				WidgetChatInputAttachments.access$getFlexInputFragment$p(stickerListener.this$0).s.hideExpressionTray();
+			} catch (Throwable ignored) {
+			}
+		}));
+	}
 
-        // Patch onClick to send sticker
-        patcher.patch(WidgetStickerPicker.class.getDeclaredMethod("onStickerItemSelected", StickerItem.class), new PreHook(param -> {
-            try {
-                // getSendability is patched above to always return SENDABLE so get the real value via reflect
-                if (ReflectUtils.getField(param.args[0], "sendability") == StickerUtils.StickerSendability.SENDABLE) return;
-
-                var sticker = ((StickerItem) param.args[0]).getSticker();
-
-                RestAPIParams.Message message = new RestAPIParams.Message(
-                    "https://media.discordapp.net/stickers/"+sticker.d()+sticker.b()+"?size="+stickerSize,
-                    Long.toString(NonceGenerator.computeNonce(ClockFactory.get())),
-                    null,
-                    null,
-                    Collections.emptyList(),
-                    null,
-                    new RestAPIParams.Message.AllowedMentions(
-                            Collections.emptyList(),
-                            Collections.emptyList(),
-                            Collections.emptyList(),
-                            false
-                    ),
-                    null,
-                    null
-                );
-
-                Utils.threadPool.execute(() -> {
-                    RxUtils.subscribe(
-                            RestAPI.getApi().sendMessage(StoreStream.getChannelsSelected().getId(), message),
-                            RxUtils.createActionSubscriber(zz -> {})
-                    );
-                });
-
-                // Skip original method
-                param.setResult(null);
-
-                // Use different approach to dismiss sticker picker
-                Object flexInputFragment = ReflectUtils.getField(param.thisObject, "flexInputFragment");
-                Object flexInputViewModel = ReflectUtils.getField(flexInputFragment, "r");
-                Method hideMethod = flexInputViewModel.getClass().getMethod("hideExpressionTray");
-                hideMethod.invoke(flexInputViewModel);
-            } catch (Throwable ignored) {
-            }
-        }));
-    }
-
-    @Override
-    public void stop(Context context) {
-        // Remove all patches
-        patcher.unpatchAll();
-    }
+	@Override
+	// Called when your plugin is stopped
+	public void stop(Context context) {
+		// Remove all patches
+		patcher.unpatchAll();
+	}
 }
+
+/*
+This plugin was mainly written as a learning exercise in order to
+figure out how to send links on tap since it would be required to
+send custom stickers. Whether or not custom stickers will ever
+happen remains to be seen...
+
+
+Copyright (C) Rhythm Lunatic 2021
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
